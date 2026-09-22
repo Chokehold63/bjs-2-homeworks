d4682b739745b5e0c3045bdd8cb396d724de2383
Neto test <kontur.prod@gmail.com>
2026-09-22 22:31:01 +0900

    complete homework_3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((element, index) => {
+        return element === arr2[index];
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filteredUsers = users.filter((user) => {
+        return user.gender === gender;
+    })
+    let ages = filteredUsers.map((user) => {
+        return user.age;
+    })
+    let sum = ages.reduce((acc, age) => {
+        return acc + age;
+    }, 0)
+    if (ages.length ===0) {
+        return 0;
+    }
+    let ave = sum / ages.length;
+    return ave;
 }
